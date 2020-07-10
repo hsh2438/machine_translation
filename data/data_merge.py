9b930ea56c566b0891631b65124cb08cb64c33bd
seokhyun <hsh2438@naver.com>
2020-07-10 16:05:02 +0000

    Update data_merge.py

--- data/data_merge.py
+++ data/data_merge.py
@@ -1,2 +1,25 @@
 import os
 import pandas
+import pickle
+
+
+def load_data(file_path):
+    excel = pandas.read_excel(open(file_path, 'rb'))
+    korean = excel['원문']
+    english = excel['번역문']
+    assert len(korean) == len(english)
+    
+    return [(korean[i], english[i]) for i in range(len(korean))]
+
+
+if __name__ == '__main__':
+    dataset = []
+
+    file_list = os.listdir('data')
+    for file_name in file_list:
+        if file_name.endswith('.xlsx'):
+            print(file_name)
+            dataset.extend(load_data(os.path.join('data', file_name)))
+    
+    print('{} pair data.'.format(len(dataset)))
+    pickle.dump(dataset, open('dataset.pickle', 'wb'))
